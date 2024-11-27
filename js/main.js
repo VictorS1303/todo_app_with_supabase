0831a0cbe4df7afe62088a0f8eb1b5e85e2f336b
Victor Schweitz <victorschweitz@hotmail.com>
2024-11-27 10:15:58 +0100

    Open Add Todo Form Button created

---
+++ js/main.js
@@ -0,0 +1,9 @@
+const openAddTodoFormBtn = document.getElementById('open_add_todo_form_btn')
+
+openAddTodoFormBtn.addEventListener('click', openAddTodoFormDialog)
+
+
+function openAddTodoFormDialog()
+{
+    console.log('Form dialog opened')
+}
