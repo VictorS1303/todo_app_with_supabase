d45674187c63ac2bf289c97c9ed8b38aee4f03fa
Victor Schweitz <victorschweitz@hotmail.com>
2024-11-27 10:45:23 +0100

    Add todo form dialog created

--- js/main.js
+++ js/main.js
@@ -1,9 +1,27 @@
+const addTodoFormDialog = document.getElementById('add_todo_form_dialog')
 const openAddTodoFormBtn = document.getElementById('open_add_todo_form_btn')
+const submitTodoBtn = document.getElementById('submit_todo_btn')
 
+// EVENT LISTENERS //
 openAddTodoFormBtn.addEventListener('click', openAddTodoFormDialog)
+submitTodoBtn.addEventListener('click', (e) => submitTodoForm(e))
 
+// FUNCTIONS //
 
 function openAddTodoFormDialog()
 {
     console.log('Form dialog opened')
+}
+addTodoFormDialog.showModal()
+
+function closeAddTodoFormDialog()
+{
+    addTodoFormDialog.close()
+}
+
+
+function submitTodoForm(e)
+{
+    e.preventDefault()
+    closeAddTodoFormDialog()
 }
